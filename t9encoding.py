43f4a34006df1ff55c7b5741a7b1fbb427ccc0e4
EkoSoftware <144835425+EkoSoftware@users.noreply.github.com>
2023-09-25 18:22:36 +0200

    Update t9encoding.py
    Added support for the swedish vowels "å","ä" and "ö".
    Also changed the user interface somewhat to make it a bit more clear as to which context you are in.

--- t9encoding.py
+++ t9encoding.py
@@ -14,11 +14,11 @@ def my_encode(string):
 
     t9_alphabet = {
         '1': '.', '11': ',', '111': '!', '1111':'?', '11111': "'",
-        '2': 'a', '22': 'b', '222': 'c',
+        '2': 'a', '22': 'b', '222': 'c', '2222': 'å', '22222': 'ä', '222222': 'ö',
         '3': 'd', '33': 'e', '333': 'f',
         '4': 'g', '44': 'h', '444': 'i',
         '5': 'j', '55': 'k', '555': 'l',
-        '6': 'm', '66': 'n', '666': 'o',
+        '6': 'm', '66': 'n', '666': 'o', 
         '7': 'p', '77': 'q', '777': 'r', '7777': 's',
         '8': 't', '88': 'u', '888': 'v',
         '9': 'w', '99': 'x', '999': 'y', '9999': 'z',
@@ -38,7 +38,7 @@ def my_decode(string):
     tempstring = ""
     t9_alphabet = {
         '1': '.', '11': ',', '111': '!', '1111':'?', '11111': "'",
-        '2': 'a', '22': 'b', '222': 'c',
+        '2': 'a', '22': 'b', '222': 'c', '2222': 'å', '22222': 'ä', '222222': 'ö',
         '3': 'd', '33': 'e', '333': 'f',
         '4': 'g', '44': 'h', '444': 'i',
         '5': 'j', '55': 'k', '555': 'l',
@@ -64,12 +64,12 @@ def my_decode(string):
 
 while True:
     os.system(clear)
-    choice = input("What would you like to do?:\n1. Encode\n2. Decode\n> ")
+    choice = input("1. Encode\n2. Decode\nWhat would you like to do?\n>")
     if choice == "1" or choice.title() == "Encode":
         user = input("Message to encode:\n> ")
         print(f"Your encoded message\n{my_encode(user)}")
     elif choice == "2" or choice.title() == "Decode":
         user = input("Message to decode:\n> ")
         print(f"Your decoded message\n{my_decode(user)}")
-    input("'q' to exit else continue\n >")
+    input("'q' to exit else Enter to continue\n >")
         
