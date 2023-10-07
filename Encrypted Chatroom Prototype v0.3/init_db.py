7801f13e150371a55927a1a1f2a62763751a0270
EkoSoftware <144835425+EkoSoftware@users.noreply.github.com>
2023-10-07 09:31:40 +0200

    Made sample passwords optional.
    Added a " if __name __ == '__main__':  " condition which works well with the changes to the server script which will import the db just to initialize it.

--- Encrypted Chatroom Prototype v0.3/init_db.py
+++ Encrypted Chatroom Prototype v0.3/init_db.py
@@ -13,19 +13,22 @@ CREATE TABLE IF NOT EXISTS userdata (
     password VARCHAR(255) NOT NULL
 )
 """)
+if __name__ == '__main__':
+    
+    username1, password1 = "doggo", hashlib.sha256("doggo".encode('utf-8')).hexdigest()
+    username2, password2 = "admin", hashlib.sha256("adminpass".encode('utf-8')).hexdigest()
+    username3, password3 = "joshua", hashlib.sha256("joshuapass".encode('utf-8')).hexdigest()
+    username4, password4 = "you", hashlib.sha256("shallnotpass".encode('utf-8')).hexdigest()
+    print(password1)
 
-username1, password1 = "doggo", hashlib.sha256("doggo".encode('utf-8')).hexdigest()
-username2, password2 = "admin", hashlib.sha256("adminpass".encode('utf-8')).hexdigest()
-username3, password3 = "joshua", hashlib.sha256("joshuapass".encode('utf-8')).hexdigest()
-username4, password4 = "you", hashlib.sha256("shallnotpass".encode('utf-8')).hexdigest()
-print(password1)
+    cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", (username1, password1))
+    cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", (username2, password2))
+    cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", (username3, password3))
+    cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", (username4, password4))
+    cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", ("textuser", "textpass"))
+    cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", ("användarnamn", "lösenord"))
 
-cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", (username1, password1))
-cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", (username2, password2))
-cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", (username3, password3))
-cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", (username4, password4))
-cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", ("textuser", "textpass"))
-cur.execute("INSERT INTO userdata (username, password) VALUES (?,?)", ("användarnamn", "lösenord"))
-
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
+else:
+    conn.close()
