ade253173e72910ae134f12ddf82d459e554a707
EkoSoftware <144835425+EkoSoftware@users.noreply.github.com>
2023-10-14 11:58:36 +0200

    Update server.py v0.8.9
    Added end to end AES encryption with the cryptography library.
    Cleaned up alot of the code.

--- Encrypted Chatroom Prototype v0.5/server.py
+++ Encrypted Chatroom Prototype v0.5/server.py
@@ -1,30 +1,55 @@
-import sqlite3
-import hashlib
-import socket
-import threading
-import os
+import sqlite3, hashlib, socket, threading, os, time
 import init_db
 from icecream import ic
-import time
+from cryptography.fernet import Fernet
+
+""" 
+Status:
+    Login funkar
+    Registrering funkar
+    Kryptering funkar
+    
+Att göra:
+#Skapa en funktion som skriver en fil med Fernet nycklar för 30 dagar -
+#-och som automatiskt väljer nyckel beroende på dag i månaden.
+#Whisper funktion
+"""
+
+myFernetKey = b'q7qDPOZInGtw50dngbM3MZdVKejQFzYPrURqaks5kxU='
+myFernetCipher = Fernet(myFernetKey)
 
 
 os.system('cls') if os.name == 'nt' else os.system('clear')
-host, port = "192.168.1.20", 9999
+host, port = "localhost", 9998
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
-admins = list()
-clients = list()
+admins    = list()
+clients   = list()
 usernames = list()
 
-print()
+# Cipher
+myKey = b'q7qDPOZInGtw50dngbM3MZdVKejQFzYPrURqaks5kxU='
+myCipher = Fernet(myKey)
+def mySend(client, text):
+    text = text.encode()
+    text = myCipher.encrypt(text)
+    return client.send(text)
 
+def myReadClient(client, *username):
+    message = client.recv(1024)
+    if message:
+        return myCipher.decrypt(message).decode()
+    else:
+        return None
+    
+# Register new users
 def myNewUser(username, password, c):
     conn = sqlite3.connect('userdata.db')
     cur = conn.cursor()
-    print('1. NewUser OsPath test:','userdata.db')
+    print('1. NewUser: OsPath test:','userdata.db')
     
     cur.execute("SELECT * FROM userdata WHERE  username = ?", (username,)) 
     print(f'2. NewUser: Requested for registration :', username)
@@ -36,21 +61,20 @@ def myNewUser(username, password, c):
         
         
         print(f'4. NewUser: {username} added!')
-        c.send(f"Registration of account {username} was successful!".encode())
+        mySend(c,f'[Server]:\nRegistration of account "{username}" was successful!')
         c.close
         cur.close()
         return
     
-    print(f'X. NewUserError: username: ', username,'was taken')
-    c.send("Username taken.".encode())
+    print(f'X. NewUserDebug: username: {username} was taken')
+    mySend(c, "Username taken.")
     cur.close()
     
-# Setup
+# Setting up connections and authentication.
 def recieve():
-    i = 1
     while True:
         client, addr = server.accept()
-        ic(f'Connected with {str(addr)}')
+        ic('Connected with',str(addr))
         
         thread = threading.Thread(target=authenticate, args=(client,))
         thread.start()
@@ -59,40 +83,49 @@ def recieve():
 def authenticate(client,):
     try:
         while True:
-            client.send("Username: ".encode())
-            username = client.recv(1024).decode()
-            if 'CLOSE'*8 == username: ic('Client has stopped the connection',client.close);break
+            # Requesting Account details
+            mySend(client, f'Username: ')
+            username = myReadClient(client)
+            if not username: client.close;break
+            
                 
-            client.send("Password: ".encode())
-            password = client.recv(1024).decode()
-            if 'CLOSE'*8 == password: ic('Client has stopped the connection',client.close);break
+            mySend(client, f'Password: ')
+            password = myReadClient(client)
+            if not password: client.close;break
+            password = hashlib.sha3_256(password.encode()).hexdigest()
             
+            # Debug
+            print(f'{username} : \nHashed password: {password}')
             
-            print(f'{username} : {password}')
-            
+            # If Client Registers...
             if username.startswith('REGISTER '): 
                 username = username.split()[1]
-                ic(myNewUser(username, password, client))
+                
+                myNewUser(username, password, client)
                 continue
+            
+            # Authenticating Login Details
             else:
                 ic("AuthDebug 1: checking logins against database...")
                 conn = sqlite3.connect('userdata.db')
                 cur = conn.cursor()
                 cur.execute("SELECT * FROM userdata WHERE username = ? AND password = ?", (username, password))
+                
 
                 if cur.fetchall():
                     ic("AuthDebug 2: Login Success")
-                    client.send("Login successful!".encode())
                     clients.append(client)
                     usernames.append(username)
+                    mySend(client,"Login successful!")
+                    
                     ic("AuthDebug 3: Broadcasting that client has joined.")
-                    broadcast(f'[Server] : {username} has joined the chat!'.encode(),whichclient=client)
+                    broadcast(f'[Server] : {username} has joined the chat!',whichclient=client)
                     
                     ic("AuthDebug 4: Sending client to 'handle_client'")
                     handle_client(client, username)
                     
                 else:
-                    client.send("Login failed!".encode())
+                    mySend(client,"Login failed!")
                 cur.close()
             
     except Exception as e: 
@@ -105,9 +138,8 @@ def handle_client(client, username):
     while True:
         try:
             while client in clients:
-                empty = False
                 ic("HandleDebug 1: Waiting for message from client")
-                msg = client.recv(1024).decode()
+                msg = myReadClient(client)
                 
                 if not msg:
                     client.close
@@ -116,27 +148,36 @@ def handle_client(client, username):
                     broadcast(f'{username} has left the chat!')
                     break
                 
-                ic("HandleDebug 2: Sending message to broadcast function.")
-                broadcast(msg, client)
-                amountSent += 1
-                ic(f"HandleDebug Messages : {amountSent}")
-            
+                match msg.split()[1]:
+                    case '': pass
+                    case __:
+                            ic("HandleDebug 2: Sending message to broadcast function.")
+                            broadcast(msg, whichclient=client)
+
+                            amountSent += 1
+                            ic(f"HandleDebug Messages : {amountSent}")
                 
         except Exception as e:
-            print(f'Handle Client ExceptionError: {e}')
+            print(f'["Handle Client" ExceptionError]:\n{e}')
 
 # BROADCAST
 def broadcast(message, whichclient=None):
-    ic("BroadcastDebug 1: Trying to broadcast message from client")
+    try:
+        originalmessage = message[:]
+        message = message.encode()
+        message = myCipher.encrypt(message)
+
+        #ic("BroadcastDebug 1: Trying to broadcast message from client")
+        for client in clients: 
+                if client != whichclient:
+                    ic(client.send(message))
+
+        print(f'[Broadcasting message]\n{originalmessage}\n[End Of Message]')
+
+        #ic("BroadcastDebug 2: Broadcast Completed on this part")
     
-    for client in clients: 
-            if client != whichclient:
-                ic(client.send(message.encode()))
-    
-    print(f'[Broadcasting message]\n{message}\n[End Of Message]')
-    
-    ic("BroadcastDebug 2: Broadcast Completed on this part")
-    
+    except Exception as e:
+        print(f'["Broadcast" ExceptionError]:\n{e}')
     
 
 # Starta Servern
