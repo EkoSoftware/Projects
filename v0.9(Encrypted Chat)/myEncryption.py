88bb6153687c255b068c27f0ff5733fd7129ddd2
EkoSoftware <144835425+EkoSoftware@users.noreply.github.com>
2023-10-18 19:33:55 +0200

    Update myEncryption.py
    Changed the encryption from messing around with bytes and byte commands in python to a much more concise code.

--- v0.9(Encrypted Chat)/myEncryption.py
+++ v0.9(Encrypted Chat)/myEncryption.py
@@ -49,46 +49,32 @@ def myGetEncryptionKey(cut):
         key += scramble
     return key[:cut]
 
-def myCipher(string, key):
-    for i, char in enumerate(string):
-        match char:
-            case 'å': char.replace('å','aoaoaoao')
-            case 'ä': char.replace('ä','aeaeaeae')
-            case 'ö': char.replace('ö','oeoeoeoe')
+def myCipher(text, key, salt=0):
+    encryption = ''
+    while len(key) < len(text):
+        key += key
+    key = key[:len(text)]
+    #print(f'Key adjusted for message: "{key}"')
     
-    length = len(key)
-    keyBytes = key.encode('utf-8')
-    strBytes = string.encode('utf-8')
+    for i, char in enumerate(text):
+        value = ord(char) + ord(key[i])
+        if salt: value += salt
+        encryption += chr(value)
     
-    encryption = []
-    for i, byte in enumerate(strBytes):
-        keyByte = keyBytes[i % length]
-        cryptedByte = (byte + keyByte) % 256
-        encryption.append(cryptedByte)
-    
-    return bytes(encryption)
-    
-def myDecipher(string, key):
-    length = len(key)
-    keyBytes = key.encode('utf-8')
-    strBytes = string
+    return encryption
 
-    decryption = []
-    for i, byte in enumerate(strBytes):
-        keyByte = keyBytes[i % length]
-        decryptedByte = (byte - keyByte) % 256
-        decryption.append(decryptedByte)
+def myDecipher(text, key, salt=0):
+    decryption = ''
+    while len(key) < len(text):
+        key += key
+    key = key[:len(text)]
+    
+    for i, char in enumerate(text):
+        value = ord(char) - ord(key[i])
+        if salt: value -= salt
+        decryption += chr(value)
 
-    decrypted_String = [chr(x) for x in decryption]
-    text = ''.join(decrypted_String)
-    
-    for i, char in enumerate(string):
-        match char:
-            case 'aoaoaoao': char.replace('aoaoaoao','å')
-            case 'aeaeaeae': char.replace('aeaeaeae','ä')
-            case 'oeoeoeoe': char.replace('oeoeoeoe','ö')
-    
-    return text
+    return decryption
 
 if __name__ == '__main__':
     from myEncryption import myGetEncryptionKey
@@ -97,9 +83,9 @@ if __name__ == '__main__':
     
     while True:
         uinput = input("Message to encrypt\n>")
-        enc = myCipher(uinput, key)
+        enc = myCipher(uinput, key, 1337)
         print(f'Encrypted message:\n{enc}')
-        dec = myDecipher(enc, key)
+        dec = myDecipher(enc, key, 1337)
         print(f'Deciphered message:\n{dec}')
         
         #uinput = input("Message to decrypt\n>")
